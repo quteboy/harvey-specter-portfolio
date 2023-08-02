5577a8f337ff98a999da07af7620a4545e33d613
DESKTOP-J1R67FM\Zaid <zaid.shaikh@clinivantage.com>
2023-08-02 22:52:50 +0530

    more changes in website

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,25 +1,60 @@
 import React from "react";
 import "./About.css";
-import { Grid, Box, Button, Stack, Typography } from "@mui/material";
+import {
+  Grid,
+  Box,
+  Button,
+  Stack,
+  Typography,
+  Card,
+  CardContent,
+} from "@mui/material";
+import data from "./aboutData.json";
+console.log("data", data);
 const About = () => {
   return (
     <React.Fragment>
       <div className="bacground-img-about">
         <Grid px={2} py={1} container direction="row" alignItems="flex-start">
-          <Grid item lg={6} md={8} sm={12} xs={12}>
+          <Grid item lg={12} md={12} sm={12} xs={12}>
             <Box
               sx={{
-                paddingTop: "30%",
+                paddingTop: "8%",
               }}
             >
               <Typography mb={1} color="white" variant="h4">
-                My name is Harvey Specter
+                Most notable Casess
               </Typography>
-              <Typography color="white" variant="body1">
-                i am practicing law since last 12 years with rock solid record
-                of winning cases in trial right now i am name partner at Specter
-                Litt{" "}
-              </Typography>
+              <Grid
+                container
+                direction="row"
+                alignItems="flex-start"
+                spacing={3}
+              >
+                {data.map((item) => {
+                  return (
+                    <Grid item lg={4} key={item.id}>
+                      <Card
+                        variant="outlined"
+                        sx={{ backgroundColor: "transparent" }}
+                        p={3}
+                      >
+                        <CardContent>
+                          <Typography color="white" variant="h5">
+                            {item.caseTitle}
+                          </Typography>
+                          <Typography color="white" variant="h6s">
+                            {item.clientName}
+                          </Typography>
+                          <Typography color="white" variant="body2">
+                            {item.description}
+                          </Typography>
+                        </CardContent>
+                      </Card>
+                    </Grid>
+                  );
+                })}
+              </Grid>
             </Box>
           </Grid>
         </Grid>
